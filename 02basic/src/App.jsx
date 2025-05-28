1e5bdd01c7a5ab39bf99c41a1287bd647783679d
Beast-1-3 <akarsh7376@gmail.com>
2025-05-28 10:46:42 +0530

    my_second_commit(2)

--- 02basic/src/App.jsx
+++ 02basic/src/App.jsx
@@ -1,7 +1,7 @@
 //import { useState } from 'react'
 
 import './App.css'
-import Card from './components/card'
+import Card from './components/Card'
 function App() {
   //const [count, setCount] = useState(0)
 let MyObj ={
