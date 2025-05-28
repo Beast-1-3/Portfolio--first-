34055239fe1978556125adf05b5fcf5b3d3f5995
Beast-1-3 <akarsh7376@gmail.com>
2025-05-28 10:39:51 +0530

    my_second_commit

---
+++ 02basic/src/App.jsx
@@ -0,0 +1,27 @@
+//import { useState } from 'react'
+
+import './App.css'
+import Card from './components/card'
+function App() {
+  //const [count, setCount] = useState(0)
+let MyObj ={
+  name:"hello",
+  age :12 
+}
+
+  return (
+    <>
+     <h1 className='bg-green-400 text-black p-4  mb-4 rounded-xl'>
+      Tailwind Test
+      
+
+     </h1>
+     
+     < Card channel="HELLO" btnon="DEMO"/>
+     <br/>
+     < Card channel="leap" btnon="faith"/>
+    </>
+  )
+}
+
+export default App
